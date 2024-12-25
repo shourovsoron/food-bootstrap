cff7a91db49d4c2072997b0a6bd9f65f8e2abe82
Shourov Soron <shourovhossainsoron@gmail.com>
2024-12-25 21:29:18 +0600

    stacky card

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,21 @@ let navCollapse = document.querySelector(".navbar-collapse.collapse");
 
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add("header__scrolled");
         navCollapse.classList.remove("show");
-    }else{
+    } else {
         nav.classList.remove("header__scrolled");
 
     }
-} 
+}
 
-if(document.documentElement.scrollTop > 20){
+if (document.documentElement.scrollTop > 20) {
     nav.classList.add("header__scrolled");
 }
 
-navBar.forEach(function (a){
-    a.addEventListener("click", function(){
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
         navCollapse.classList.remove("show");
 
     })
@@ -59,90 +59,150 @@ gsap.ticker.lagSmoothing(0);
 
 
 
+// Stacky Card Animation===========
+
+// Initialize GSAP animation
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".stacky__card");
+
+    cards.forEach((cardd, i) => {
+
+        const card = cards[i];
+        let scale = 1,
+            rotate = 0;
 
 
-// ========For Text Reavel Animation=========
+        if (i !== cards.length - 1) {
+            scale = 0.7 + 0.06 * i;
+            rotate = -10
+        }
 
-const allReveal = document.querySelectorAll('.revel__text');
 
-allReveal.forEach((revelText) => {
 
-    const split_text = new SplitType(revelText, {types: 'chars'});
+        // Stacking animation
+        gsap.to(card, {
 
-    gsap.from(split_text.chars, {
+            scale: scale,
+            rotationX: rotate,
+            transformOrigin: "top center",
+            ease: 'none',
 
-        scrollTrigger: {
-            trigger: revelText,
-            scroller: "body",
-            // markers: true,
-            start: 'top 90%',
-            end: 'top 60%',
-            scrub: 2,
-        },
+            scrollTrigger: {
+                trigger: card,
+                start: `top ${(150 + 10 * i)}`,
+                end: 'bottom 600',
+                endTrigger: '.end-anim',
+                pin: card,
+                pinSpacing: false,
+                scrub: true,
+                // markers: true
 
-        x: 3,
-        opacity: 0.7,
-        stagger: .009,
-        duration: 0.1,
-        color: '#6e6e6e89'
 
-    });
+            },
 
+        });
+
+    })
+
+
+
+    // On hover, add rotation effect
 
 });
 
 
 
-// ===== For Text Dancing animation=====
-const allDancing = document.querySelectorAll('.dancing-text');
 
-allDancing.forEach((dancingText) => {
 
-    const dancing_text = new SplitType(dancingText, {types: 'chars'});
+// ========For Text Reavel Animation=========
+document.addEventListener('DOMContentLoaded', () => {
 
-    gsap.from(dancing_text.chars, {
 
-        scrollTrigger: {
-            trigger: dancingText,
-        },
 
-        y: -50,
-        opacity: 0.7,
-        stagger: .1,
-        duration: 3,
-        ease: "elastic.out(1.2,0.2)",
-        color: '#6e6e6e89'
+    const allReveal = document.querySelectorAll('.revel__text');
+
+    allReveal.forEach((revelText) => {
+
+        const split_text = new SplitType(revelText, { types: 'chars' });
+
+        gsap.from(split_text.chars, {
+
+            scrollTrigger: {
+                trigger: revelText,
+                scroller: "body",
+                // markers: true,
+                start: 'top 90%',
+                end: 'top 60%',
+                scrub: 2,
+            },
+
+            x: 3,
+            opacity: 0.7,
+            stagger: .009,
+            duration: 0.1,
+            color: '#6e6e6e89'
+
+        });
+
 
     });
 
 
-})
+
+    // ===== For Text Dancing animation=====
+    // const allDancing = document.querySelectorAll('.dancing-text');
+
+    // allDancing.forEach((dancingText) => {
+
+    //     const dancing_text = new SplitType(dancingText, { types: 'chars' });
+
+    //     gsap.from(dancing_text.chars, {
+
+    //         scrollTrigger: {
+    //             trigger: dancingText,
+    //         },
+
+    //         y: -50,
+    //         opacity: 0.7,
+    //         stagger: .1,
+    //         duration: 3,
+    //         ease: "elastic.out(1.2,0.2)",
+    //         color: '#6e6e6e89'
+
+    //     });
+
+
+    // })
 
 
 
 
 
-// For Button Animation
-let allBtn = document.querySelectorAll('.btn-animation');
+    // For Button Animation
+    let allBtn = document.querySelectorAll('.btn-animation');
 
-allBtn.forEach((btn) => {
+    allBtn.forEach((btn) => {
 
-    btn.addEventListener('mouseenter', ()=>{
-        gsap.to(btn, {
+        btn.addEventListener('mouseenter', () => {
+            gsap.to(btn, {
 
-            background: 'linear-gradient(to right, var(--primary-color) 100%, transparent 0%)',
-            ease: "power3.out",
-            duration: .7
+                background: 'linear-gradient(to right, var(--primary-color) 100%, transparent 0%)',
+                ease: "power3.out",
+                duration: .7
+            })
+        });
+
+        btn.addEventListener('mouseleave', () => {
+            gsap.to(btn, {
+                background: 'linear-gradient(to left, var(--primary-color) 0%, transparent 0%)',
+                ease: "power3.out",
+                duration: .7
+
+            })
         })
-    });
 
-    btn.addEventListener('mouseleave', ()=>{
-        gsap.to(btn, {
-            background: 'linear-gradient(to left, var(--primary-color) 0%, transparent 0%)',
-            ease: "power3.out",
-            duration: .7
-
-        })
     })
+
 
 })
